ceadcc6c10c7dd0e7f9b21aa2b2548bcc9e547b6
Brock Reece <brockreece@gmail.com>
2022-07-04 09:40:29 +0100

    Update plugin for Vue 3

--- src/wasm.js
+++ src/wasm.js
@@ -1,16 +1,7 @@
-import Vue from 'vue';
-
-const extractModule = async (module) => {
-  const { instance } = await module();
-  return instance.exports;
-};
-
-export default async (vue, options = {}) => {
-  Vue.prototype.$wasm = {};
-  await Object.keys(options.modules)
-    .forEach(async (key) => {
-      const extracted = await extractModule(options.modules[key]);
-      Vue.prototype.$wasm[key] = extracted;
-    });
-  await Vue.nextTick();
-};
+export default = {
+  install(app, options) {
+    const wasm = options.modules
+    app.config.globalProperties('$wasm', wasm)
+    app.provide('$wasm', wasm)
+  }
+}
